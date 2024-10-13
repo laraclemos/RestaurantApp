3aae01206c46bf2c50a4d99e876b3fb869345963
laraclemos <laraclemos@gmail.com>
2024-10-13 14:03:08 -0700

    Finished one section with the food information

--- app/src/main/java/com/example/restaurantapp/model/Food.java
+++ app/src/main/java/com/example/restaurantapp/model/Food.java
@@ -1,2 +1,40 @@
-package com.example.restaurantapp.model;public class Food {
+package com.example.restaurantapp.model;
+
+public class Food {
+    int imgFood;
+    String foodName;
+    String foodDescription;
+    String price;
+
+    public int getImgFood() {
+        return imgFood;
+    }
+
+    public void setImgFood(int imgFood) {
+        this.imgFood = imgFood;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getFoodDescription() {
+        return foodDescription;
+    }
+
+    public void setFoodDescription(String foodDescription) {
+        this.foodDescription = foodDescription;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
